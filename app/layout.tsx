30f7af016833d928f24f5ddb065b68f539fae652
Janis <31632325+thinkjanis@users.noreply.github.com>
2024-10-30 22:38:53 +0200

    Render GA only in production

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
         <FaviconLinks />
       </head>
       <body className="font-sans">{children}</body>
-      <GoogleAnalytics gaId="G-MSPNG3FNBM" />
+      {process.env.APP_ENV === 'production' && (
+        <GoogleAnalytics gaId={process.env.GA_ID} />
+      )}
     </html>
   )
 }
